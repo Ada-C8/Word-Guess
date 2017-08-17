1f9582198fe35a980a5e32c79cdeb6b8da359f89
Tanisha <tanishahampden@gmail.com>
2017-08-17 14:56:13 -0700

    updates

--- random_word.rb
+++ random_word.rb
@@ -1,16 +1,18 @@
 class RandomWord
+  attr_reader :word
   def initialize()
     word_arr = ["envelope", "cat", "dragon", "coffee", "water", "pencil"]
-    @word = word_arr[rand(word_arr.length)]
+    @word = word_arr[rand(word_arr.length)].split('')
     @length = @word.length
   end
 
-  def writes_underscore()
+  def writes_underscore
     underscore = "_  " * @length
-    return underscore
+    return underscore.split('  ')
+  end
+
+  def reprint
+
   end
 
 end
-
-penny = RandomWord.new
-puts penny.writes_underscore
