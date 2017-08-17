e577f5373623c65f69afd799dea4f0d278fb0b92
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-08-17 14:58:43 -0700

    add guesses visual

--- main.rb
+++ main.rb
@@ -8,3 +8,9 @@ File.open("dictionary.txt", "r").each do |line|
 end
 
 game_word = dictionary_array.sample.split("")
+
+# display progress
+correct_guesses = Array.new(game_word.length, "a").join
+dashes = Array.new(game_word.length, "-").join
+puts correct_guesses
+puts dashes
