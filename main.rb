3731abbd0386970578ce535cb2efc7d6f9de6cd3
Tanisha <tanishahampden@gmail.com>
2017-08-17 16:53:16 -0700

    add cake take away to game

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 require 'artii'
 require_relative 'user_input'
 require_relative 'random_word'
+require_relative 'ascii_art'
 
 penny = RandomWord.new
 user = UserInput.new
+cake = AsciiArt.new
 # print penny.writes_underscore
 puts "random word array : #{penny.word}"
 # print penny.underscore
@@ -15,7 +17,7 @@ puts "random word array : #{penny.word}"
 
 until penny.underscore == penny.word
   puts ">> " # for testing
-  user.check_letter(penny, user)
+  user.check_letter(penny, user, cake)
   # if penny.underscore == penny.word
   #   return
   # end
