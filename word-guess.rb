f28062538d8b3f100a649875da8cd773148e111a
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-08-17 14:37:19 -0700

    added guess method to word class

--- word-guess.rb
+++ word-guess.rb
@@ -1,22 +1,39 @@
 require 'random-word'
+RandomWord.exclude_list.push(/_/, /\'/, /-/)
 
 #word class for unique words
 class Word
-  attr_reader :word
+  attr_accessor :guesses
+  attr_reader :word, :letters, :num
+
   def initialize
     if [1, 2, 3].sample == 3
       @word = RandomWord.adjs.next
     else
       @word = RandomWord.nouns.next
     end
+    @letters = word.split("")
+    @num = @letters.length
+    @guesses = []
   end
 
   def display
 
   end
 
+  def guess
+    @guess = gets.chomp.to_s.downcase
+    until ('a'..'z').to_a.include?(@guess)
+      puts "Please enter a single letter."
+      @guess = gets.chomp.to_s.downcase
+    end
+  end
+end #end Word class
 
 
-end
-word = Word.new
-puts word.word
+attempt = Word.new
+puts attempt.word
+attempt.display
+print attempt.letters
+puts attempt.num
+attempt.guess
