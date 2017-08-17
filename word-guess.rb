20df488ff477fe005594d3ff5d5802ba904c25dd
mslaura.mae@gmail.com <mslaura.mae@gmail.com>
2017-08-17 15:54:59 -0700

    [KE/LR] Successful loop

--- word-guess.rb
+++ word-guess.rb
@@ -8,6 +8,7 @@ class WordGuess
   end
 
   def word_bank
+    tries = 10
     word_list = ["Accio", "Patronus", "Wand", "Hermione", "Snitch", "Dobby"]
     our_word = word_list.sample.upcase
     puts our_word
@@ -15,37 +16,27 @@ class WordGuess
 
     puts "Welcome to our wonderful word guessing game! You have 10 tries to guess the word, one letter at a time!"
     print "Whats your first letter guess? (a through z): "
-    letter = gets.chomp.upcase
-    puts letter
-    wrong_guess = []
 
-    if our_word.include? letter
-      puts "You guessed right!"
-    else
-      wrong_guess << letter
-      puts "Try again!"
+    until tries == 0
+      letter = gets.chomp.upcase
+      puts letter
+      wrong_guess = []
+
+      if our_word.include? letter
+        puts "You guessed right!"
+      else
+        wrong_guess << letter
+        puts "Try again!"
+        tries -= 1
+      end
     end
-  end
 
-  # def ask_letter
-  #   puts "Welcome to our wonderful word guessing game! You have 10 tries to guess the word, one letter at a time!"
-  #   print "Whats your first letter guess? (a through z): "
-  #   letter = gets.chomp.upcase
-  #   puts letter
-  #   wrong_guess = []
-  #
-  #   if our_word.include? letter
-  #     puts "You guessed right!"
-  #   else
-  #     wrong_guess << letter
-  #     puts "Try again!"
-  #     puts wrong_guess
-  #   end
-  #
-  #   # 1.times do ('a'..'z').include? letter
-  #   #   #puts "true"
-  #   # end
-  # end
+    if tries == 0
+      puts "Game over!"
+      exit
+    end
+
+  end
 
 
 end
