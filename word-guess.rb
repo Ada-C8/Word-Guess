2b16789c56e9d071b6ff1c0899de3cac374f4b0d
mslaura.mae@gmail.com <mslaura.mae@gmail.com>
2017-08-17 16:32:40 -0700

    [KE/LR] String made to compare

--- word-guess.rb
+++ word-guess.rb
@@ -9,24 +9,27 @@ class WordGuess
 
   def word_bank
     tries = 10
-    word_list = ["Accio", "Patronus", "Wand", "Hermione", "Snitch", "Dobby"]
-    our_word = word_list.sample.upcase
-    puts our_word
+    #word_list = ["Accio", "Patronus", "Wand", "Hermione", "Snitch", "Dobby"]
+    our_word = 'DOBBY'
+    #our_word = word_list.sample.upcase
+    our_word_split = our_word.scan(/\w/).uniq.flatten.sort
     #puts our_word.chars.each_slice(1).to_a
 
     puts "Welcome to our wonderful word guessing game! You have 10 tries to guess the word, one letter at a time!"
     print "Whats your first letter guess? (a through z): "
+    wrong_guess = []
+    right_guess = []
 
     until tries == 0
       letter = gets.chomp.upcase
-      puts letter
-      wrong_guess = []
 
       if our_word.include? letter
+        right_guess << letter
         puts "You guessed right!"
       else
         wrong_guess << letter
         puts "Try again!"
+        p wrong_guess
         tries -= 1
       end
     end
