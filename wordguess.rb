6c711c3179c3f635a9ad2eadfbca22171081a4ca
murog <guillermina.muro@gmail.com>
2017-08-17 14:26:37 -0700

    added user interface

--- wordguess.rb
+++ wordguess.rb
@@ -1,42 +1,42 @@
 require "faker"
 
-class random_word
-attr_reader :word
+class Random_Word
+  attr_reader #:word
 
-def initialize
-  @word = "#{Faker::Overwatch.unique.hero}"
-  @letters = @word.split('')
-end
-
-def letter_index(user_letter)
-  letter_indeces =[]
-  @letters.length.times do |i|
-    if @letters[i] == user_letter
-      letter_indeces << i
-    end
+  def initialize
+    @word = "#{Faker::Overwatch.unique.hero}"
+    @letters = @word.split('')
   end
-  return letter_indeces
-end
 
-def default_display
-  puts "_ " * @word.length
-end
-def update_display
-  puts
+  def letter_index(user_letter)
+    letter_indeces =[]
+    @letters.length.times do |i|
+      if @letters[i] == user_letter
+        letter_indeces << i
+      end
+    end
+    return letter_indeces
+  end
 
-
-end
-
-
-class user_inputs
-  attr_reader
-  attr_writer
-
-end
+  def default_display
+    puts "_ " * @word.length
+  end
+  def update_display
+    puts
 
 
-def initialize
-end
+  end
+
+
+  class user_inputs
+    attr_reader
+    attr_writer
+
+  end
+
+
+  def initialize
+  end
 
 end
 
@@ -44,20 +44,45 @@ class game
   attr_reader
   attr_writer
 
-def initialize
-end
+  def initialize
+  end
 
-def flower
-end
+  def flower
+  end
 
-def remove_flower
-end
+  def remove_flower
+  end
 
-def cat
-end
+  def cat
+  end
 
-def cat_move
+  def cat_move
 
-end
+  end
   POT
+end
+
+
+puts "Welcome to Word Guess!  Let me think of a word first..... ok got it"
+puts "You can guess wrong 5 times until the cat eats you"
+random_word = Random_Word.new
+cat_position0
+guess = 0
+random_word.word_display
+
+until guess == 5
+  puts "guess one letter"
+  # refactor into a method
+  until user_input.length == 1 && user_input.to_i == 0
+    user_input = gets.chomp
+  end
+
+  if random_word.letter_index(user_input).length > 0
+    puts "nice"
+    random_word.word_display(indeces)
+  else
+    puts "yikes"
+    c += 1
+    cat_position(guess)
+  end
 end
