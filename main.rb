4a9f3bef599443e9caa414bd2fee43bfe3d2185f
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-17 14:33:35 -0700

    create comments on intial structure

---
+++ main.rb
@@ -0,0 +1,33 @@
+#  Word Game
+
+
+# Intro to user
+# selects difficulty ?
+# word bank -
+  # maybe in its own class maynot,
+  # maybe one word bank depending difficulty?
+# Find word in word bank.
+
+
+# Display game board to user
+  # Image/ASCII art
+  # Blank letter spaces
+  # Letters already guessed?
+# Ask user to guess a letter
+# Store the user input
+  # CHeck array for guessed
+  # If it's in array ask for another guess.
+  # If not then store in array.
+  #   check if guess is in word.
+  #   Update screen accordingly.
+  #     Add letter
+  #     take flower away (tries).
+# Loop ends:
+  # If tries run out.
+  #   Show a losing screed
+  # Or if whole word is guessed.
+  #   Show a winning screen.
+
+# Ask user if they want to play again.
+#   if yes then loop to begining of Game
+#   Or no then display a thank and goodbye.
