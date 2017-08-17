84cf9c1e5c6a415662a173a80f90013704074274
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-08-17 15:06:53 -0700

    [ID/CS] Completed check_letter method

--- Word-Guess.rb
+++ Word-Guess.rb
@@ -4,25 +4,26 @@ class Party
   def initialize
     @word = Faker::Color.color_name
     @word_array = @word.to_a
+    @dash_word = "_ " * @word.length
     @score = 0
     @wrong_guess = 0
     @max_wrong_guess = 3
     @won = false
-    @right_letters = []
+    #@right_letters = []
   end
 
-  def check_letter(letter)
-    if @word.include? letter
-      @right_letters << letter
-      dash_word(letter)
+  def check_letter(guess_letter)
+    if @word_array.include? guess_letter
+      @word_array.length.times do |index|
+        if word_array[index] == guess_letter
+          @dash_word[index * 2] = guess_letter
+        end
+      end
     else
       @wrong_guess += 1
     end
     return @word.include? letter
   end
 
-  def dash_word(letter)
 
-
-  end
 end
