1680669dd3a6d98db8b2958d00443ffa81c3ed4f
mslaura.mae@gmail.com <mslaura.mae@gmail.com>
2017-08-17 14:56:41 -0700

    [KE/LR] Word bank method added

--- word-guess.rb
+++ word-guess.rb
@@ -1,1 +1,20 @@
 # Word guess game - Kayla and Laura
+
+class WordGuess
+  attr_reader :game
+
+  def initialize
+    @game = game
+  end
+
+  def word_bank
+    word_list = ["Accio", "Patronus", "Wand", "Hermione", "Snitch", "Dobby"]
+    our_word = word_list.sample
+    puts our_word
+    puts our_word.chars.each_slice(1).to_a
+  end
+
+end
+
+game = WordGuess.new
+game.word_bank
