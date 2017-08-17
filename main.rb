8f66ff9eaa9f1e3443c37cb0bb6c4041736d9efe
Tanisha <tanishahampden@gmail.com>
2017-08-17 14:55:45 -0700

    update main.rb to include new classes

--- main.rb
+++ main.rb
@@ -1,1 +1,10 @@
 require 'artii'
+require_relative 'user_input'
+require_relative 'random_word'
+
+penny = RandomWord.new
+charming = UserInput.new
+print penny.writes_underscore
+print penny.word
+
+charming.check_letter(penny)
