263d31b3a23a4283331cdb0ce2f99539e50eaa33
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-08-17 14:45:55 -0700

    convert word string to array of letters

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@
 # require_relative "dictionary.txt"
 
 # pull words from dictionary.txt
-dictionary_array = ["test"]
+dictionary_array = []
 File.open("dictionary.txt", "r").each do |line|
   dictionary_array.push(line.chomp)
 end
 
-print dictionary_array
+game_word = dictionary_array.sample.split("")
