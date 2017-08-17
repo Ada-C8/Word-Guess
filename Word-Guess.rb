aad2f96f31d30076a342730b6bc951d0eef53b93
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-08-17 15:33:17 -0700

    [ID/CS] Tested .check_letter and added game_over method

--- Word-Guess.rb
+++ Word-Guess.rb
@@ -1,14 +1,15 @@
 require 'faker'
 class Party
 
+  attr_accessor :word, :word_array, :dash_word, :score, :wrong_guess, :max_wrong_guess
+
   def initialize
     @word = Faker::Color.color_name
-    @word_array = @word.to_a
+    @word_array = @word.split("")
     @dash_word = "_ " * @word.length
     @score = 0
     @wrong_guess = 0
-    @max_wrong_guess = 3
-    @won = false
+    @max_wrong_guess = 5 #easy = 5, medium, hard = 3
     #@right_letters = []
   end
 
@@ -19,11 +20,33 @@ class Party
           @dash_word[index * 2] = guess_letter
         end
       end
+      @score += 1
     else
-      @wrong_guess += 1
+      if game_over
+        puts "game over!!!"
+      else
+        @wrong_guess += 1
+      end
     end
-    return @word.include? letter
+    return @word.include? guess_letter
   end
 
+  def game_over
+    return @wrong_guess == @max_wrong_guess
+  end
 
 end
+
+# party1 = Party.new
+# puts party1.word
+# puts party1.dash_word
+#
+# party1.check_letter("a")
+# party1.check_letter("e")
+# party1.check_letter("i")
+# party1.check_letter("o")
+# party1.check_letter("u")
+#
+# puts party1.wrong_guess
+# puts party1.score
+# puts party1.dash_word
