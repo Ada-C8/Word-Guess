e06caca4e7e8768ab3b8af8db034be22007e9822
Tanisha <tanishahampden@gmail.com>
2017-08-17 15:19:25 -0700

    test new methods

--- main.rb
+++ main.rb
@@ -4,7 +4,10 @@ require_relative 'random_word'
 
 penny = RandomWord.new
 charming = UserInput.new
-print penny.writes_underscore
+# print penny.writes_underscore
 print penny.word
+print penny.underscore
 
 charming.check_letter(penny)
+
+penny.reprint(charming)
