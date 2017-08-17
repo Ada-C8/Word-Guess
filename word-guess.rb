8683c709980ae576c3847a690320787435d7c669
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-08-17 15:41:15 -0700

    Added more methods to Game and Word classes [IS]

--- word-guess.rb
+++ word-guess.rb
@@ -2,25 +2,51 @@ require 'faker'
 
 class Word
 
-  initialize(word: Faker::Dessert.variety)
+  attr_reader :word, :letters, :blanks
+
+  def initialize(word: Faker::Dessert.variety)
     @word = word
     @letters = word.split("")
-    @blanks = "_" * @letters.length
+    @blanks = "-" * @letters.length
   end
 
-  def contains?(letter_guess)
-    return @letters.include?(letter_guess)
+  # def contains?(letter_guess)
+  #   return @letters.include?(letter_guess)
+  # end
+
+  def replace_blanks(letter_guess)
+    indices = find_indices(letter_guess)
+
+    indices.each do |index|
+      @blanks[index].replace letter_guess
+    end
   end
 
+  private
+
+  def find_indices(letter_guess)
+    indices = []
+
+    (0..@letters.length).each do |index|
+      if letter_guess == @letters[index]
+        indices << index
+      end
+    end
+
+    return indices
+    # @letters.each do |letter|
+    #   if letter_guess == letter
+
+  end
 
 
 end #end of class
 
 class Game
 
-  initialize
+  def initialize
     @art = newart
-    @word = Word.new
+    @game_word = Word.new
     @guesses = []
     @turns = 0
   end
@@ -34,13 +60,33 @@ class Game
       puts "You guessed that letter already!"
       return
     end
-    if @word.contains?(user_guess)
+    if @game_word.contains?(user_guess)
       Game.reveal
     else
       @turns += 1
       Art.decompose
     end
     @guesses << user_guess
+  end
+
+
+  def reveal(user_guess)
+    @blanks.replace_blanks(user_guess)
+    puts @game_word.blanks
+  end
+
+  def lose
+    if @turns > 5
+      puts "You lose :("
+      exit
+    end
+  end
+
+  def win
+    if !@blanks.include?("-")
+      puts "You win!"
+      exit
+    end
   end
 
 end # end of class
