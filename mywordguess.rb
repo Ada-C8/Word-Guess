12393a0b52a0588d0af45ab6199fcda1b17c1085
Tanja Stroble <plusdeltah@gmail.com>
2017-08-17 15:07:11 -0700

    method for underscores to appear in array works

--- mywordguess.rb
+++ mywordguess.rb
@@ -1,1 +1,19 @@
-some change 
+class Word
+  attr_accessor :random_word
+  def initialize (random_word)
+    @random_word = random_word
+  end
+
+  def word_display
+    word_display = []
+    random_word.length.times do
+      # i.gsub("i", "_")
+      word_display.push "_"
+    end
+    return word_display
+  end
+
+end
+
+tanja = Word.new("Tanja")
+print tanja.word_display
