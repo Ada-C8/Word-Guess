f3e432c0a3eb68dc4b2a9a4450575b03658907ae
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-08-17 15:35:00 -0700

    update display method

--- word-guess.rb
+++ word-guess.rb
@@ -18,11 +18,27 @@ class Word
   end
 
   def display
-
+    num_correct = 0
+    @letters.each do |letter|
+      if @guesses.include?(letter)
+        print letter + " "
+        num_correct += 1
+      else
+        print "_ "
+      end
+    end
+    print "\n"
+    if num_correct == @num
+      puts "You guessed the word! WOOO!"
+      exit
+    end
+    num_correct = 0
   end
 
   def guess
+    puts "Please guess a letter!"
     @guess = gets.chomp.to_s.downcase
+    # check if the guess is a valid guess
     valid = 0
     until valid == 1
       if is_letter?(@guess) && !already_guessed?(@guess)
@@ -33,15 +49,12 @@ class Word
           puts "You already chose that letter! Please choose another."
         else
           puts "Please enter a single letter."
-
         end
         @guess = gets.chomp.to_s.downcase
       end
     end
-
-
-
-
+    # check if the guess is in the word
+    return @letters.include?(@guess)
   end
 
   private
@@ -59,9 +72,30 @@ end #end Word class
 attempt = Word.new
 puts attempt.word
 attempt.display
-print attempt.letters
-puts attempt.num
-attempt.guess
-attempt.guess
-attempt.guess
-attempt.guess
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
+attempt.display
+puts attempt.guess
