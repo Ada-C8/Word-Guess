129ca90cb0a61cd7003727256720f891e5c41515
Eva Sawyer <esawyer25@gmail.com>
2017-08-17 15:18:52 -0700

    add method for add_guess

--- word_class.rb
+++ word_class.rb
@@ -25,11 +25,24 @@ class WordClass
     @gameboard = Array.new(@word.length, "_")
     @number_of_guesses = 0
     @used_guesses = []
+    @guess_ind = []
   end
 
+  def add_guess(new_guess)
+    @used_guesses << new_guess
+  end
+
+  def output_gameboard
+    puts @gameboard.join
+  end
+
+  def update_gameboard
+
+  end
   def include_letter?(input)
-    gues_ind = (0 ... @word.length).find_all { |i| @word[i,1] == input}
-    return gues_ind
+    @guess_ind = (0 ... @word.length).find_all { |i| @word[i,1] == input}
+
+    return @guess_ind
   end
 
 end
