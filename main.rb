2076699e05994c7a50608bbb5940d55791cb6a3a
Tanisha <tanishahampden@gmail.com>
2017-08-17 16:11:51 -0700

    add logic/loop to run game

--- main.rb
+++ main.rb
@@ -5,11 +5,27 @@ require_relative 'random_word'
 penny = RandomWord.new
 user = UserInput.new
 # print penny.writes_underscore
-print penny.word
-print penny.underscore
+puts "random word array : #{penny.word}"
+# print penny.underscore
 
-user.check_letter(penny, user)
-print penny.underscore
-user.get_next_letter
-user.check_letter(penny, user)
-# penny.rewrite_underscore(user)
+# user.check_letter(penny, user)
+# user.get_next_letter
+# user.check_letter(penny, user)
+# user.get_next_letter
+
+until penny.underscore == penny.word
+  puts ">> " # for testing
+  user.check_letter(penny, user)
+  # if penny.underscore == penny.word
+  #   return
+  # end
+  # user.get_next_letter
+end
+
+
+
+
+
+
+
+#penny.rewrite_underscore(user)
