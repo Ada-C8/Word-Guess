a717d9bd66c0c255780b4906233f431640dab47e
Lauren <Laurenelee88@gmail.com>
2017-08-17 16:31:16 -0700

    write method for display partial answer

--- word_guess.rb
+++ word_guess.rb
@@ -1,10 +1,12 @@
 class Game
-
+  attr_reader :random_word, :partial_answer
   def initialize
-    @generate_answer = generate_answer
+    generate_answer
     # @guess = guess
     @incorrect_guess = 0
-
+    @partial_answer = ["_", "_", "_", "_", "_"]
+    @game = true
+    # @input = input
   end
 
   def generate_answer
@@ -12,17 +14,19 @@ class Game
     @random_word = @random_word.sample.chars
 
   end
+  def partial_answer
+    return @partial_answer
+  end
 
-
-  def check_guess
-    if @random_word.include? guess
-      # call art method
-      # AND display_partial_answer
+  def check_guess(input)
+    display_partial_answer(input)
+    display_art
+    if @random_word.include? input
     else
-      # call art method
       add_incorrect_guess
     end
   end
+
 
   def add_incorrect_guess
     @incorrect_guess+=1
@@ -32,42 +36,107 @@ class Game
   def add_correct_guess
   end
 
-  def display_art
-  end
+  def display_partial_answer(input)
+    @random_word.each_with_index do |letter, index|
+      if input == letter
+        puts index
+        @partial_answer[index] = input
 
-  def display_partial_answer
+        print @partial_answer
+      end
+    end
   end
 
   def end_game
-    when @incorrect_guess=10
-      break
+    if @incorrect_guess==10
+      game_live = false
+      # break
+    end
   end
 
-end # end class
+  def display_art
+    case @incorrect_guess
+    when 0
+      return  "
+      .    .
+      )  (
+      _ _ _ _ _ _ _ _ _ _ _   (.--.)
+      {{ { { { { { { { { { { ( ^_^ )
+        >>>>>>>>>>>>>>>>>>>>>>>>>>-->
+        WOW! You guessed correctly!
+        "
+      when 1
+        return "
+        .    .
+        )  (
+        _ _ _ _ _ _ _ _ _   (.--.)
+        { { { { { { { { { { ( ^_^ )
+          >>>>>>>>>>>>>>>>>>>>>>>-->
+          9 guesses left
+          "
+        when 2
+          return "
+          .    .
+          )  (
+          _ _ _ _ _ _ _ _   (.--.)
+          { { { { { { { { { ( ^_^ )
+            >>>>>>>>>>>>>>>>>>>>>-->
+            8 guesses left.
+            "
+          when 3
+            return "
+            .    .
+            )  (
+            _ _ _ _ _ _ _   (.--.)
+            { { { { { { { { ( ^_^ )
+              >>>>>>>>>>>>>>>>>>>-->
+              7 guesses left.
+              "
+            when 4
+              return ""
+            when 5
+              return ""
+            when 6
+              return ""
+            when 7
+              return ""
+            when 8
+              return ""
+            when 9
+              return ""
+            else
+              return ""
+            end
+          end
 
-# actual game play
+          # end # end class
 
-game1 = Game.new
-puts game1.generate_answer
-game_live = true
+          # actual game play
 
-while game_live
-puts "Guess a letter"
-guess = gets.chomp
+          game1 = Game.new
+          puts "Welcome to Word Guess!"
 
-# game2 = Game.new
-# puts game2.generate_answer
+          print game1.generate_answer
+          print game1.partial_answer
+          puts "Please choose a letter"
+          input = gets.chomp
+          puts game1.check_guess(input)
+          # puts game1.display_partial_answer
 
 
-
-
-end
-
-puts "
-{o}{o}{o}
-|  |  |
-\|  \|  \|
-[~~~~~~~~~]
-|~~~~~~~|
-|_______|
-"
+          #
+          # puts game1.generate_answer
+          # game_live = true
+          # guess = ""
+          #
+          # # guess.include? @random_word
+          #
+          # while game_live
+          #   puts "Guess a letter"
+          #   guess = gets.chomp
+          #
+          #   # game2 = Game.new
+          #   # puts game2.generate_answer
+          #
+          # end
+        end
