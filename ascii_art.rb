2cce1f52cf281ad201503ada6ed0070fbacffa6b
Tanisha <tanishahampden@gmail.com>
2017-08-17 16:45:16 -0700

    add take_away_candle method (like a boss)

--- ascii_art.rb
+++ ascii_art.rb
@@ -1,27 +1,32 @@
 class AsciiArt
   attr_reader :body, :candle_light, :candle_body
+  attr_writer :candle_light, :candle_body
   def initialize
     @body = [
-"  @@@@@@@@@
+"    |||||
+  @@@@@@@@@
   {~@~@~@~}
 @@@@@@@@@@@@@
 {~@ HAPPY @~}
 {  BIRTHDAY }
 @@@@@@@@@@@@@"
 ]
-    @candle_light = ["    `````"]
-    @candle_body = ["    |||||"] #don't move
+    # @candle_light = ["    `````"] #don't move
+    # @candle_body = ["    |||||"] #don't move
+    @candle_light = "    `````" #don't move
 
 
   end
 
   def change_candles
+    a = @candle_light.split('')
+    a.pop
+    @candle_light = a.join
+  end
 
+  def take_away_candle
+    change_candles
+    puts @candle_light
+    puts @body
   end
 end
-
-test1 = AsciiArt.new
-
-puts test1.candle_light
-puts test1.candle_body
-puts test1.body
