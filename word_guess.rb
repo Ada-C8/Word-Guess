6a563cc18079a0a5c7797a25f4219005e25f39e5
Lauren <Laurenelee88@gmail.com>
2017-08-17 14:37:53 -0700

    create class game

--- word_guess.rb
+++ word_guess.rb
@@ -1,8 +1,27 @@
 class Game
 
+  def initialize
+    @generate_answer = generate_answer
+    # @guess = guess
+  end
+
+  def generate_answer
+    @random_word = ["issue", "union", "refer", "bowel", "fence", "great", "valid", "urine", "glove", "money", "brick"]
+    @random_word = @random_word.sample
+  end
+
+end # end class
+
+# actual game play
+
+game1 = Game.new
+puts game1.generate_answer
 
 
-end
+
+
+
+
 
 puts "
  {o}{o}{o}
