3cdd47760b2e2b94a9c18bbdb9ff994423c1d3aa
mslaura.mae@gmail.com <mslaura.mae@gmail.com>
2017-08-17 15:30:51 -0700

    [KE/LR] Added letter scanner

--- word-guess.rb
+++ word-guess.rb
@@ -9,12 +9,50 @@ class WordGuess
 
   def word_bank
     word_list = ["Accio", "Patronus", "Wand", "Hermione", "Snitch", "Dobby"]
-    our_word = word_list.sample
+    our_word = word_list.sample.upcase
     puts our_word
-    puts our_word.chars.each_slice(1).to_a
+    #puts our_word.chars.each_slice(1).to_a
+
+    puts "Welcome to our wonderful word guessing game! You have 10 tries to guess the word, one letter at a time!"
+    print "Whats your first letter guess? (a through z): "
+    letter = gets.chomp.upcase
+    puts letter
+    wrong_guess = []
+
+    if our_word.include? letter
+      puts "You guessed right!"
+    else
+      wrong_guess << letter
+      puts "Try again!"
+    end
   end
 
+  # def ask_letter
+  #   puts "Welcome to our wonderful word guessing game! You have 10 tries to guess the word, one letter at a time!"
+  #   print "Whats your first letter guess? (a through z): "
+  #   letter = gets.chomp.upcase
+  #   puts letter
+  #   wrong_guess = []
+  #
+  #   if our_word.include? letter
+  #     puts "You guessed right!"
+  #   else
+  #     wrong_guess << letter
+  #     puts "Try again!"
+  #     puts wrong_guess
+  #   end
+  #
+  #   # 1.times do ('a'..'z').include? letter
+  #   #   #puts "true"
+  #   # end
+  # end
+
+
 end
+
+
+
+
 
 game = WordGuess.new
 game.word_bank
