bb63c902df44caf9d96fbf9dfa1ee6b7a4b91098
Eva Sawyer <esawyer25@gmail.com>
2017-08-17 15:58:49 -0700

    Add game_status and update_guess_count methods

--- word_class.rb
+++ word_class.rb
@@ -23,7 +23,7 @@ class WordClass
   def initialize(word)
     @word = word.downcase
     @gameboard = Array.new(@word.length, "_")
-    @number_of_guesses = 0
+    @guess_count = 0
     @used_guesses = []
     @guess_ind = []
   end
@@ -46,19 +46,40 @@ class WordClass
 
   def include_letter?
     @guess_ind = (0 ... @word.length).find_all { |i| @word[i,1] == @used_guesses[-1]}
-
     return @guess_ind
   end
 
+  def gameboard_complete
+    !@gameboard.include?("_")
+  end
+
+  def update_guess_count
+    @guess_count =+ 1
+  end
+
+  def game_status
+    if  gameboard_complete
+      puts "You won"
+    elsif @guess_count < 5 && !gameboard_complete
+      puts "Please enter another guess"
+    elsif @guess_count >= 5
+      puts "You lost"
+    end
+
+
+  end
 end
 
 
-d= WordClass.new("cactus")
 
-puts d.word
-puts d.gameboard
-puts d.add_guess("c")
-puts d.include_letter?
-puts d.output_gameboard
-puts d.update_gameboard
-puts d.output_gameboard
+  d= WordClass.new("cactus")
+
+  puts d.word
+  puts d.gameboard
+  puts d.add_guess("c")
+  puts d.include_letter?
+  puts d.output_gameboard
+  puts d.update_gameboard
+  puts d.output_gameboard
+  puts d.gameboard_complete
+  puts d.game_status
