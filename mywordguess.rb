4709c9cefee7852c1a7f99f2a44f19781ea244c9
Tanja Stroble <plusdeltah@gmail.com>
2017-08-18 16:49:14 -0700

    bad letter added

--- mywordguess.rb
+++ mywordguess.rb
@@ -4,6 +4,7 @@ class Word
     @random_word = random_word.split("")
     @word_display = Array.new(random_word.length, "_ ") #make array of underscores
     @num_bad_guess = 0 #had to give it initial val so that we cound increment it with count bad guess method
+    @bad_letters = []
   end
 
  def reveal(letter)
@@ -13,6 +14,8 @@ class Word
       if alpha == letter
         @word_display[counter] = letter
         @changed = true # indicates that guess was right
+      else
+         @bad_letter << letter
       end
       counter += 1
     end
@@ -22,7 +25,7 @@ class Word
   def count_bad_guess
     if @changed == nil #checks to see if guess was wrong
       @num_bad_guess += 1 # if guess is wrong increments bad guess otherwise  gives "free" guess
-      print "You have made #{@num_bad_guess} bad guesses which include ()." #prints to user how many bad guesses they took
+      print "You have made #{@num_bad_guess} bad guesses which include #{@bad_letter.join("")}." #prints to user how many bad guesses they took
     end
     return @num_bad_guess #otherwise method returns nil
   end
