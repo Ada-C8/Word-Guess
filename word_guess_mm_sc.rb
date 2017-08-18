b8f52f86163e419d659544429401b7172f0930d6
Marisa Morris <marisanmorris@gmail.com>
2017-08-18 12:57:07 -0700

    THE VOID OF KNOWLEDGE HAS BEEN FILLER.

--- word_guess_mm_sc.rb
+++ word_guess_mm_sc.rb
@@ -1,5 +1,5 @@
 class Game
-attr_reader :hang_art
+attr_reader :hang_art, :letter_array, :under_score, :guess_index, :guess, :guess_check
   def initialize(letter_array)
     @letter_array = letter_array
     @under_score  = under_score
@@ -10,12 +10,15 @@ attr_reader :hang_art
 
 # --------------------------------
   def guess_check
+      @guess_index = []
       @letter_array.each do |letter|
         if @guess == letter_array.include?(@guess)
-          @guess_index = @letter_array.index(@guess)
-          return @guess_index
+          @guess_index << @letter_array.index(@guess)
+
         end
+
     end
+   return @guess_index
   end
 
     def user_interface
@@ -33,6 +36,8 @@ attr_reader :hang_art
         x
       end
     end
+    under_score = @under_score
+    return under_score
   end
 # -------------------------
 
@@ -52,24 +57,49 @@ end
 
 
 class Word
-  attr_reader :get_word
+  attr_reader :word_bank, :game_word, :letter_array, :get_word
   def initialize(word_bank)
     @word_bank = word_bank
+    @game_word = game_word
+    @letter_array = letter_array
+
   end
 
-  def get_word
+  def get_word(word_bank)
     @game_word = @word_bank.delete(@word_bank.sample)
     @letter_array = @game_word.split(//)
-    return @letter_array
+    letter_array = @letter_array
+    return letter_array
   end
 
 end
 
 
-word_bank = Word.new(["acorn", "acute", "bitty", "cooky", "doozy"])
+word_bank = Word.new(["acorn", "acute", "aitty", "aooky", "aoozy"])
+letter_array = word_bank.get_word(word_bank)
 
 # word_bank.get_word
 new_game = Game.new(letter_array)
+new_game.user_interface
+new_game.guess_check
+new_game.replace_index
+puts new_game.replace_index
 
 
-puts word_bank.get_word
+#puts word_bank.get_word
+# guess = "a"
+# guess_index = []
+# letter_array = ["a","b", "a"]
+# under_score = ["_","_","_"]
+# letter_array.each do |letter|
+#   if guess == letter_array.include?(guess)
+#     guess_index << letter_array.index(guess)
+#   end
+# end
+# puts guess_index
+# under_score = under_score.map do |x|
+#   if x == guess_index
+#     x = guess
+#   end
+# end
+# puts under_score
