63de9cb1d81247e98c1fa246088f72466a1ab4ac
Tanisha <tanishahampden@gmail.com>
2017-08-17 21:29:11 -0700

    add user prompts for right and wrong guesses

--- user_input.rb
+++ user_input.rb
@@ -13,12 +13,14 @@ class UserInput
     # if random word array includes @current_letter
     if class_rw.word.include?(@current_letter)
       @right_letters << @current_letter
+      puts "Good job, keep guessin'"
       # replace underscore at that index with user_input(@current_letter)
       class_rw.rewrite_underscore(user_input)
       # print @right_letters
       return
     else
       @wrong_letters << @current_letter
+      puts "WRONG!, you loss a candle"
 
       cake.take_away_candle
       # print @wrong_letters
