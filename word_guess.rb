4076d934a3cbdf576c2f4676bfe7ff71930433f7
Eva Sawyer <esawyer25@gmail.com>
2017-08-17 20:45:52 -0700

    made the initial layout of the user interface

--- word_guess.rb
+++ word_guess.rb
@@ -1,1 +1,17 @@
 require_relative 'word_class.rb'
+
+new_game = WordClass.new
+word = new_game.generate_word
+puts ###########################
+puts "I am thinking of a word that has #{word.length} letters"
+puts "Guess the letters in the word I am thinking of."
+game = "on"
+while game == "on"
+
+  #complete ascii goes here
+  puts "Ascii goes here!"
+  puts d.output_gameboard
+  30.time do |n|
+      puts d.turn
+  end
+end
