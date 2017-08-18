55b845c60ba425d490f2fea302a4dc53560e7ea9
Tanja Stroble <plusdeltah@gmail.com>
2017-08-17 17:06:32 -0700

    Figured out how to replace element of the array from underscore to letter provided, otherwise leave as is

--- mywordguess.rb
+++ mywordguess.rb
@@ -1,35 +1,42 @@
 class Word
-  attr_accessor :random_word
+  attr_accessor :random_word, :word_display
   def initialize (random_word)
     @random_word = random_word.split("")
+    @word_display = word_display
   end
 
   def word_display
-    word_display = []
+    @word_display = []
     random_word.length.times do
       # i.gsub("i", "_")
-      word_display.push "_"
+      @word_display.push " _ "
     end
-    return word_display
+    return @word_display
   end
 
   def reveal(letter)
-    if @random_word.include?(letter)
-      @random_word.each do |i|
-        if i == letter
-          word_display[i] = letter
-        end
+    counter = 0
+    @random_word.each do |alpha|
+      if alpha == letter
+        @word_display[counter] = letter
       end
+      counter += 1
     end
-    return word_display
+    return @word_display
   end
-
 end
 
 # tanja = Word.new("Tanja")
 # print tanja.word_display
 
+# letters = ["G", "a", "l", "e"]
+
+# numbers = ["_ ", "_ ", "_ ", "_ "]
+
+# letter = "a"
+
 
 
 gale = Word.new("Gale")
-print gale.reveal("a")
+print gale.reveal("y")
+print gale.reveal("y").join
