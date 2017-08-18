4406f7fd676ad7a32e34f6e4b53511036e528e14
Tanisha <tanishahampden@gmail.com>
2017-08-17 22:15:28 -0700

    change random words for easy testing

--- random_word.rb
+++ random_word.rb
@@ -2,7 +2,7 @@ class RandomWord
   attr_reader :word, :underscore
 
   def initialize()
-    word_arr = ["envelopea", "cat", "dragon", "coffeea", "water", "pencila"]
+    word_arr = ["art", "cat", "dragon", "nap", "water", "sat"]
     @word = word_arr[rand(word_arr.length)].split('')
     @length = @word.length
     @underscore = ("_  " * @length).split('  ')
