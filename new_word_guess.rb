2c63cac5b7ec30602e9bfe2528ed2185f3877157
Severin Wiggenhorn <severin.randall@gmail.com>
2017-08-18 15:23:26 -0700

    Added colorize and winning/losing functionality

--- new_word_guess.rb
+++ new_word_guess.rb
@@ -1,20 +1,43 @@
+require 'colorize'
+
 class Game
 
   def initialize(answer_array)
+    puts "Welcome to the sushi word game! If you run out of sushi before you guess the word, you lose!!".colorize(:green)
     @answer = answer_array.sample
     @correct_guesses = []
     @incorrect_guesses = []
-    @tries = 5
+    @tries = 6
 
     @answer_as_array = split_answer(@answer)
 
     while @tries > 0
-      accept_user_guess
-      display
-      # if
-    end
+      if @tries > 1
+        accept_user_guess
+        display
+        if correct_display == @answer
+          puts "~WINNER~~WINNER~~WINNER~".colorize(:light_magenta)
+          exit
+        end
 
+      elsif @tries == 1
+        puts "You LOST! You LOST! You LOST!".colorize(:light_red)
+        exit
+      end
+    end
   end
+
+  def sushi_display
+    <<~SUSHI
+
+    ,'' ;  ;  ; ''|||\///
+    ',,_;__;__;__;,'''/\\\\
+    |           |
+    '.________.'
+    SUSHI
+  end
+
+
 
   private
 
@@ -24,12 +47,17 @@ class Game
 
   def accept_user_guess
     puts "Please enter a letter"
-    user_input = gets.chomp
+    user_input = gets.chomp.downcase
+
     if @answer_as_array.include?(user_input)
       @correct_guesses << user_input
     else
-      @incorrect_guesses << user_input
-      @tries -= 1
+      if @incorrect_guesses.include?(user_input)
+        puts "You already guessed that letter! No penalty, but try not to do it again!".colorize(:light_green)
+      else
+        @incorrect_guesses << user_input
+        @tries -= 1
+      end
     end
   end
 
@@ -46,9 +74,12 @@ class Game
   end
 
   def display
-    puts "Correct guesses: #{correct_display}"
-    puts "Incorrect guesses: #{@incorrect_guesses}"
-    puts "You have this many tries left: #{@tries}"
+    puts "Number of sushi pieces == number of tries remaining"
+    @tries.times do
+      print sushi_display
+    end
+    puts "Correct guesses: #{correct_display}".colorize(:light_blue)
+    puts "Incorrect guesses: #{@incorrect_guesses}".colorize(:red)
   end
 
 end
@@ -63,8 +94,6 @@ class Guess
 
 end
 
-
 answer_array = ["bat"]
-
 
 new_game = Game.new(answer_array)
