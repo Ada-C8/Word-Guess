95cc3e4bc688464cc3cf410da75c0da7e5f2926f
Tanja Stroble <plusdeltah@gmail.com>
2017-08-17 18:45:57 -0700

    added game class that needs work

--- mywordguess.rb
+++ mywordguess.rb
@@ -26,6 +26,35 @@ class Word
   end
 end
 
+class Game
+  attr_accessor :random_word
+  def initialize
+    @win = false
+    @guesses_taken = 0
+    @guesses_allowed = 5  #random_word.length , does it need to be an argument in initialize?
+
+    #when guesses_taken > guesses_allowed  game over
+    #or
+    #when guesses_taken == guesses_allowed && word_display.includes? " _ " game over
+    #when guesses_taken == guesses_allowed && random_word != word_display game over
+  end
+
+  def play
+    word = Word.new("faker")
+    while @win == false
+      @guesses_allowed.times do
+        puts "Please guess the letter"
+        guess = gets.chomp.downcase
+        word.reveal(guess) #
+        @guesses_taken += 1
+        if @random_word == @word_display #is not comparing
+          @win = true
+          puts "Congrats!"
+        end
+      end
+    end
+  end
+end
 # tanja = Word.new("Tanja")
 # print tanja.word_display
 
@@ -35,11 +64,14 @@ end
 
 # letter = "a"
 
+puts "welcome to the game"
+
+word = Game.new.play
 
 
-gale = Word.new("foobarfoo")
-print gale.reveal("o")
-print gale.reveal("f")
+# gale = Word.new("foobarfoo")
+# print gale.reveal()
+# print gale.reveal("f")
 
-
-print gale.word_display
+#
+# print gale.word_display
