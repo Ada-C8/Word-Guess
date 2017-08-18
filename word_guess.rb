989873e926dbd11fa659a9e8cc29f9f2b716ead1
Eva Sawyer <esawyer25@gmail.com>
2017-08-17 22:09:42 -0700

    added the formatting to make the user interface prettier

--- word_guess.rb
+++ word_guess.rb
@@ -1,17 +1,25 @@
 require_relative 'word_class.rb'
 
 new_game = WordClass.new
-word = new_game.generate_word
-puts ###########################
-puts "I am thinking of a word that has #{word.length} letters"
-puts "Guess the letters in the word I am thinking of."
+new_game.start_game
+puts "THIS IS FOR DEBUGGING! THE WORD IS: #{new_game.word}"#just for debugging
+
+puts "\n**************************************************".colorize(:white)
+puts "******************  WORD GAMES  ******************".colorize(:white)
+puts "**************************************************\n\n".colorize(:white)
+
+puts "I am thinking of a word that has #{new_game.word.length} letters\n".colorize(:white)
+puts "Guess letters to see if you can guess the word!".colorize(:white)
+
 game = "on"
 while game == "on"
-
-  #complete ascii goes here
-  puts "Ascii goes here!"
-  puts d.output_gameboard
-  30.time do |n|
-      puts d.turn
+  30.times do |n|
+    puts "\n\n**************************************************".colorize(:white)
+    puts "***************  #{6-new_game.guess_count} guesses left!  ****************".colorize(:white)
+    puts "**************************************************".colorize(:white)
+    puts "Ascii goes here!\n\n"
+    new_game.output_gameboard
+    new_game.turn
   end
+
 end
