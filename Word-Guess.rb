dcacf9d1edbf1ef0ef974f77435c787b56cd5c98
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-08-17 17:08:24 -0700

    [ID/CS] Met all basic requirements and added ascii art

--- Word-Guess.rb
+++ Word-Guess.rb
@@ -9,8 +9,60 @@ class Party
     @dash_word = "_ " * @word.length
     @score = 0
     @wrong_guess = 0
-    @max_wrong_guess = 50 #easy = 5, medium, hard = 3
+    @max_wrong_guess = 5 #easy = 5, medium, hard = 3
     @guessed_letters = []
+  end
+
+  def ascii_skull
+    skull1 = <<HEREDOC
+    .... NO! ...                  ... MNO! ...
+    ..... MNO!! ...................... MNNOO! ...
+    ..... MMNO! ......................... MNNOO!! .
+    .... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .
+HEREDOC
+
+    skull2 = <<HEREDOC
+    ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....
+    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...
+    ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....
+    ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...
+    ....... MMMM..     OPPMMP      .,OMI! ....
+HEREDOC
+
+    skull3 = <<HEREDOC
+    ...... MMMM::    o.,OPMP,.o    ::I!! ...
+        .... NNM:::.,,OOPM!P,.::::!! ....
+         .. MMNNNNNOOOOPMO!!IIPPO!!O! .....
+        ... MMMMMNNNNOO:!!:!!IPPPPOO! ....
+          .. MMMMMNNOOMMNNIIIIIPPPOO!! ......
+HEREDOC
+
+    skull4 = <<HEREDOC
+         ...... MMMONNMMNNNIIIOOO!..........
+      ....... MN MOMMMNNNIIIIIO! OO ..........
+    ......... MNO! IiiiiiiiiiiiI OOOO ...........
+    ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........
+HEREDOC
+
+    skull5 = <<HEREDOC
+    .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........
+    ...... MNNNNO! .. PPPPPPPPP .. MMNON!........
+     ...... OO! ................. ON! .......
+        ................................
+HEREDOC
+
+    case @wrong_guess
+      when 1
+        puts skull1
+      when 2
+        puts skull1, skull2
+      when 3
+        puts skull1, skull2, skull3
+      when 4
+        puts skull1, skull2, skull3, skull4
+      when 5
+        puts skull1, skull2, skull3, skull4, skull5
+    end
   end
 
   def check_letter(guess_letter)
@@ -25,6 +77,7 @@ class Party
       @score += 1
     else
       @wrong_guess += 1
+      ascii_skull
     end
 
     if winner
@@ -49,17 +102,18 @@ class Party
 end
 
 party1 = Party.new
-puts "Welcome to Word Guess!!!!!!"
-print "Guess a letter: "
+puts "\n\nWelcome to Word Guess!!!!!!\n\n"
 puts party1.dash_word
+puts "\n\nGuess a letter: "
 guess = gets.chomp
 party1.check_letter(guess)
 
 until party1.game_over || party1.winner
-  puts "Letters Guessed:"
-  puts party1.guessed_letters
+  puts "\n\nLetters Guessed:\n"
+  print party1.guessed_letters
+  puts "\n"
   puts party1.dash_word
-  print "Guess a letter: "
+  print "\n\nGuess a letter: "
   guess = gets.chomp
   party1.check_letter(guess)
 
