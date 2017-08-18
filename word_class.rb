918bad1a12897b7602762479aff8912ba6e2b3a2
Eva Sawyer <esawyer25@gmail.com>
2017-08-17 19:52:53 -0700

    added comments to our methods

--- word_class.rb
+++ word_class.rb
@@ -30,6 +30,8 @@ class WordClass
     @guess_ind = []
   end
 
+  #generate_word uses RandomWord gem to create a random english word of between
+  #4 and 8 characters.  It excludes words that have "-" and "_" as characters.
   def generate_word
     @word = RandomWord.nouns(not_shorter_than: 4, not_longer_than: 8).next
     while @word.include?("_") || word.include?("-")
@@ -38,61 +40,74 @@ class WordClass
     return @word
   end
 
+  #add_guesses takes the user's guess as input, then adds it to an array
+  #containing all the letters that have been guessed.  It displays that array.
   def add_guess(new_guess)
     @used_guesses << new_guess
     puts "guess count #{@guess_count}"
-    puts "You have made these guesses #{show_guesses}"
+    puts "You have made these guesses #{@used_guesses.join}"
   end
 
-  def show_guesses
-    return @used_guesses.join
-  end
-
-  def output_gameboard
-    puts @gameboard.join(" ")
-  end
-
+  #update_gameboard: In the case of a correct guess, it add the guessed letter
+  #to the gameboard.  In the case of an incorrect guess, it adds 1 to the
+  #guess_count.  It then outputs the gameboard.
   def update_gameboard
     include_letter?
     if @guess_ind.length != 0
       @guess_ind.each do |i|
         @gameboard[i] = @used_guesses[-1]
       end
+    else
       update_guess_count
     end
     output_gameboard
   end
 
+  #output_gameboard flattens the gameboard array for better viewing
+  def output_gameboard
+    puts @gameboard.join(" ")
+  end
+
+  #include_letter? returns an array with the index of all instances of the
+  #guessed letter.
   def include_letter?
     @guess_ind = (0 ... @word.length).find_all { |i| @word[i,1] == @used_guesses[-1]}
     return @guess_ind
   end
 
+  #gameboard_complete checks if the game has been won (there are no blanks left)
   def gameboard_complete
     !@gameboard.include?("_")
   end
 
+  #update_guess_count adds 1 to the guess_count
   def update_guess_count
     @guess_count += 1
   end
 
+  #game_status checks if the game has been won, lost, or is still ongoing.
+  #if the game has been won, or lost, the game exits, otherwise it keeps going.
   def game_status
     if  gameboard_complete
-      puts "You won"
+      puts "You won!"
+      exit
     elsif @guess_count < 5 && !gameboard_complete
       puts "Please enter another guess"
     elsif @guess_count >= 5
-      puts "You lost"
+      puts "You lost... the correct answer was"
+      puts @word.upcase
       exit
     end
   end
 
+  #turn combines many of the methods in this class to encaspulate a single round
+  #of the game
   def turn(input)
     puts add_guess(input)
     puts update_gameboard
     puts game_status
   end
-
+  
 
 end
 
@@ -109,7 +124,10 @@ puts d.output_gameboard
 puts d.turn("c")
 puts d.turn("g")
 puts d.turn("a")
-
+puts d.turn("e")
+puts d.turn("i")
+puts d.turn("t")
+puts d.turn("f")
 # puts d.add_guess("a")
 # puts d.update_gameboard
 # puts d.game_status
