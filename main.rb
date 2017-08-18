1221091fc7e039954a129e62b4a97649becc2443
Tanisha <tanishahampden@gmail.com>
2017-08-17 22:48:46 -0700

    check for loser and exit game

--- main.rb
+++ main.rb
@@ -21,46 +21,26 @@ puts "Welcome to Word-Guess!".colorize(:light_blue)
 puts "=".colorize(:light_blue) * 22
 puts "GAME INSTRUCTIONS:".colorize(:green)
 puts "1.) Guess the word I'm thinking for a chance to have my Birthday Cake.".colorize(:yellow)
-puts "2.) For every incorrect letter you choose, I will blow out one candle. You have 5 chances to guess.".colorize(:yellow)
+puts "2.) For every incorrect letter you choose, I will blow out one candle.".colorize(:yellow)
+puts "3.) If you guess incorrectly 5 times, I will happliy keep my cake."
 puts "READY...".colorize(:red)
 puts "SET...".colorize(:red)
-puts "GUESS!!".colorize(:red)
 
-
-
-until penny.underscore == penny.word
+# loops until underscore array == random word || length of wrong_letters == 5
+until penny.underscore == penny.word || user.wrong_letters.length == 5
   puts "Guess >> "
 
+  # check user input letter
   user.check_letter(penny, user, cake)
-  # checks for loser
+
+  # checks for loser and exits game
   if user.wrong_letters.length == 5
     puts "YOU LOSE, I WIN"
   end
-
-  # if penny.underscore == penny.word
-  #   return
-  # end
-  # user.get_next_letter
 end
-
-# puts "wrong letters : #{user.wrong_letters}"
-# puts "penny.word : #{penny.word}"
-# puts "wrong letters length: #{user.wrong_letters.length}"
-# puts "penny.word length: #{penny.word.length}"
-
-
 
 # checks for winner and outputs win message
 if penny.underscore == penny.word
   puts "YOU WIN!!!. You can eat my birthday cake!".colorize(:blue)
   cake.show_cake
 end
-
-
-
-
-
-
-
-
-#penny.rewrite_underscore(user)
