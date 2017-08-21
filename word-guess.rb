68a671377868df05c31e72691f344e0252770590
Laura Robertson <mslaura.mae@gmail.com>
2017-08-20 17:12:34 -0700

    [KE/LR] Modified methods and instance variables

--- word-guess.rb
+++ word-guess.rb
@@ -2,20 +2,17 @@
 require "colorize"
 
 class WordGuess
-  attr_reader :game
 
   def initialize
-    @game = game
     @tries = 10
+    @word_list = ["Accio", "Patronus", "Wand", "Hermione", "Snitch", "Dobby",
+      "Azkaban", "Butterbeer", "Dementor", "Dumbledore", "Snape"]
+    @our_word = @word_list.sample.upcase
   end
 
-  def word_bank
-
-    word_list = ["Accio", "Patronus", "Wand", "Hermione", "Snitch", "Dobby"]
-    our_word = word_list.sample.upcase
-    our_word_split = our_word.scan(/\w/).uniq.flatten.sort
-
-    word_split = our_word.scan(/\w/)
+  def game
+    our_word_compare = @our_word.scan(/\w/).uniq.sort
+    word_split = @our_word.scan(/\w/)
     empty_array = Array.new(word_split.length) {'_'}
 
     puts "Welcome to our wonderful word guessing game! You have 10 tries to guess the word, one letter at a time!"
@@ -23,46 +20,17 @@ class WordGuess
     wrong_guess = []
     right_guess = []
 
-
     until @tries == 0
-      puts "\n"
-      puts "      .--.          " + "                    " + "         BBBB.  "
-      puts "      |  |          " + "                    " + "        \e[1mOO--\e[0mBB  "
-      if @tries == 10
-        puts "      MMMM          " + "        \\|/         ".green + "         L__B'  "
-        puts "     /MMMMMMMMMO====" + "--------->".green + "<---------".red + "=====ONNNNNNNN  "
-        puts "    /MMMMM\\MMMM    " + "         /|\\        ".red + "          NNNNN "
-      elsif @tries > 7
-        puts "      MMMM          " + "          \\|/       ".green + "         L__B'  "
-        puts "     /MMMMMMMMMO====" + "----------->".green + "<-------".red + "=====ONNNNNNNN  "
-        puts "    /MMMMM\\MMMM    " + "           /|\\      ".red + "          NNNNN "
-      elsif @tries > 5
-        puts "      MMMM          " + "            \\|/     ".green + "         L__B'  "
-        puts "     /MMMMMMMMMO====" + "------------->".green + "<-----".red + "=====ONNNNNNNN  "
-        puts "    /MMMMM\\MMMM    " + "             /|\\    ".red + "          NNNNN "
-      elsif @tries > 3
-        puts "      MMMM          " + "              \\|/   ".green + "         L__B'  "
-        puts "     /MMMMMMMMMO====" + "--------------->".green + "<---".red + "=====ONNNNNNNN  "
-        puts "    /MMMMM\\MMMM    " + "               /|\\  ".red + "          NNNNN "
-      else
-        puts "      MMMM          " + "                \\|/ ".green + "         L__B'  "
-        puts "     /MMMMMMMMMO====" + "----------------->".green + "<-".red + "=====ONNNNNNNN  "
-        puts "    /MMMMM\\MMMM    " + "                 /|\\".red + "          NNNNN "
-      end
-      puts "   /MMMMM   \\MM    " + "                    " + "          NNNNN "
-      puts "  /MMMMMM     \\    " + "                    " + "         DDD DD "
-      puts " /MMMMMMM           " + "                    " + "       DDD  DD  "
-      puts "/MMMMMMMM           " + "                    " + "     DDDD  DDD  "
-      puts "\n"
+      puts ascii_art
       puts "Guess this word:".blue
       puts empty_array.join(" ")
       puts "You've already guessed these letters:".red
       puts wrong_guess.join(", ")
       letter = gets.chomp.upcase
 
-      if our_word.include? letter
+      if @our_word.include? letter
         right_guess << letter
-        right_guess = right_guess.uniq.flatten.sort
+        right_guess = right_guess.uniq.sort
         puts "You guessed right!"
 
         word_split.each_index do |i|
@@ -71,20 +39,9 @@ class WordGuess
           end
         end
 
-        if right_guess == our_word_split
-          puts our_word
-          puts "\n"
-          puts "
-     )   )         (                            (       )        (   (
-  ( /(( /(       ( )\ )        (       (  (     )\\ ) ( /(  (     )\\ ))\\ )
-  )\\())\\())    ( )(()/((       )\\      )\\))(   (()/( )\\()) )\\   (()/(()/(
- ((_)((_)\\     )((_/(_))\\   ((((_)(   ((_)()\\ ) /(_)((_)((((_)(  /(_)/(_))
-__ ((_)((_) _ ((_)(_))((_)   )\\ _ )\\  _(())\\_)((_))  _((_)\\ _ )\\(_))(_))_
-\\ \\ / / _ \\| | | || _ | __|  (_)_\\(_) \\ \\((_)/ |_ _||_  /(_)_\\(_| _ \\|   \\
- \\ V | (_) | |_| ||   | _|    / _ \\    \\ \\/\\/ / | |  / /  / _ \\ |   /| |) |
-  |_| \\___/ \\___/ |_|_|___|  /_/ \\_\\    \\_/\\_/ |___|/___|/_/ \\_\\|_|_\\|___/".red
-          puts "\n"
-          puts "You WIN!"
+        if right_guess == our_word_compare
+          puts @our_word
+          puts ascii_art_win
           exit
         end
 
@@ -93,9 +50,62 @@ __ ((_)((_) _ ((_)(_))((_)   )\\ _ )\\  _(())\\_)((_))  _((_)\\ _ )\\(_))(_))_
         puts "Try again!"
         @tries -= 1
       end
+
+      if @tries == 0
+        puts ascii_art_lose
+        exit
+      end
     end
 
-    if @tries == 0
+  end
+
+  def ascii_art
+    puts "\n"
+    puts "      .--.                                       BBBB.  "
+    puts "      |  |                                      \e[1mOO--\e[0mBB  "
+    if @tries > 8
+      puts "      MMMM          " + "        \\|/         ".green + "         L__B'  "
+      puts "     /MMMMMMMMMO====" + "--------->".green + "<---------".red + "=====ONNNNNNNN  "
+      puts "    /MMMMM\\MMMM    " + "         /|\\        ".red + "          NNNNN "
+    elsif @tries > 6
+      puts "      MMMM          " + "          \\|/       ".green + "         L__B'  "
+      puts "     /MMMMMMMMMO====" + "----------->".green + "<-------".red + "=====ONNNNNNNN  "
+      puts "    /MMMMM\\MMMM    " + "           /|\\      ".red + "          NNNNN "
+    elsif @tries > 4
+      puts "      MMMM          " + "            \\|/     ".green + "         L__B'  "
+      puts "     /MMMMMMMMMO====" + "------------->".green + "<-----".red + "=====ONNNNNNNN  "
+      puts "    /MMMMM\\MMMM    " + "             /|\\    ".red + "          NNNNN "
+    elsif @tries > 2
+      puts "      MMMM          " + "              \\|/   ".green + "         L__B'  "
+      puts "     /MMMMMMMMMO====" + "--------------->".green + "<---".red + "=====ONNNNNNNN  "
+      puts "    /MMMMM\\MMMM    " + "               /|\\  ".red + "          NNNNN "
+    else
+      puts "      MMMM          " + "                \\|/ ".green + "         L__B'  "
+      puts "     /MMMMMMMMMO====" + "----------------->".green + "<-".red + "=====ONNNNNNNN  "
+      puts "    /MMMMM\\MMMM    " + "                 /|\\".red + "          NNNNN "
+    end
+    puts "   /MMMMM   \\MM                                  NNNNN "
+    puts "  /MMMMMM     \\                                 DDD DD "
+    puts " /MMMMMMM                                      DDD  DD  "
+    puts "/MMMMMMMM                                    DDDD  DDD  "
+    puts "\n"
+  end
+
+  def ascii_art_win
+    puts "
+)   )         (                            (       )        (   (
+( /(( /(       ( )\ )        (       (  (     )\\ ) ( /(  (     )\\ ))\\ )
+)\\())\\())    ( )(()/((       )\\      )\\))(   (()/( )\\()) )\\   (()/(()/(
+((_)((_)\\     )((_/(_))\\   ((((_)(   ((_)()\\ ) /(_)((_)((((_)(  /(_)/(_))
+__ ((_)((_) _ ((_)(_))((_)   )\\ _ )\\  _(())\\_)((_))  _((_)\\ _ )\\(_))(_))_
+\\ \\ / / _ \\| | | || _ | __|  (_)_\\(_) \\ \\((_)/ |_ _||_  /(_)_\\(_| _ \\|   \\
+ \\ V | (_) | |_| ||   | _|    / _ \\    \\ \\/\\/ / | |  / /  / _ \\ |   /| |) |
+  |_| \\___/ \\___/ |_|_|___|  /_/ \\_\\    \\_/\\_/ |___|/___|/_/ \\_\\|_|_\\|___/".red
+    puts "\n"
+    puts "You WIN!"
+  end
+
+  def ascii_art_lose
       puts "      .--.          " + "                    " + "             "
       puts "      |  |          " + "                    " + "             "
       puts "      MMMM          " + "                    " + "     /       ".blink
@@ -106,27 +116,25 @@ __ ((_)((_) _ ((_)(_))((_)   )\\ _ )\\  _(())\\_)((_))  _((_)\\ _ )\\(_))(_))_
       puts " /MMMMMMM           " + "                    " + "   * ".blink+"####".black.on_green+"  * ".blink
       puts "/MMMMMMMM           " + "                    " + "    "+"#######".black.on_green+"  "
       puts "\n"
-      puts '                  ______                    '.red.on_black.blink
-      puts '               .-"      "-.                 '.red.on_black.blink
-      puts '              /            \                '.yellow.on_black.blink
-      puts ' _           |              |          _    '.yellow.on_black.blink
-      puts '( \          |,  .-.  .-.  ,|         / )   '.light_green.on_black.blink
-      puts ' > "=._      | )(__/  \__)( |     _.=" <    '.light_green.on_black.blink
-      puts '(_/"=._"=._ |/     /\     \| _.="_.="\_)    '.green.on_black.blink
-      puts '        "=._ (_     ^^     _)"_.="          '.green.on_black.blink
-      puts '            "=\__|IIIIII|__/="              '.cyan.on_black.blink
-      puts '           _.="| \IIIIII/ |"=._             '.cyan.on_black.blink
-      puts ' _     _.="_.="\          /"=._"=._     _   '.blue.on_black.blink
-      puts '( \_.="_.="     `--------`     "=._"=._/ )  '.blue.on_black.blink
-      puts ' > _.="                            "=._ <   '.magenta.on_black.blink
-      puts '(_/                                    \_)  '.magenta.on_black.blink
+      puts '                    ______                    '.red.on_black.blink
+      puts '                 .-"      "-.                 '.red.on_black.blink
+      puts '                /            \                '.yellow.on_black.blink
+      puts '   _           |              |          _    '.yellow.on_black.blink
+      puts '  ( \          |,  .-.  .-.  ,|         / )   '.light_green.on_black.blink
+      puts '   > "=._      | )(__/  \__)( |     _.=" <    '.light_green.on_black.blink
+      puts '  (_/"=._"=._ |/      /\     \| _.="_.="\_)   '.green.on_black.blink
+      puts '          "=._ (_     ^^     _)"_.="          '.green.on_black.blink
+      puts '              "=\__|IIIIII|__/="              '.cyan.on_black.blink
+      puts '             _.="| \IIIIII/ |"=._             '.cyan.on_black.blink
+      puts '   _     _.="_.="\          /"=._"=._     _   '.blue.on_black.blink
+      puts '  ( \_.="_.="     `--------`     "=._"=._/ )  '.blue.on_black.blink
+      puts '   > _.="                            "=._ <   '.magenta.on_black.blink
+      puts '  (_/                                    \_)  '.magenta.on_black.blink
+      puts "Sorry, the word was: #{@our_word}"
       puts "Game over!"
-      exit
-    end
-
   end
 
 end
 
-game = WordGuess.new
-game.word_bank
+newgame = WordGuess.new
+newgame.game
